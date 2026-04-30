7875953b77c272886ea92d359fa38f406249e256
mahdihasanprogrammer <mahdihasan.code@gmail.com>
2026-04-30 12:46:51 +0600

    add proxy.js file for porfile and view details page protection

---
+++ src/proxy.js
@@ -0,0 +1,25 @@
+import { headers } from 'next/headers'
+import { NextResponse } from 'next/server'
+import { auth } from './lib/auth'
+
+// This function can be marked `async` if using `await` inside
+export async function proxy(request) {
+    const session = await auth.api.getSession({
+        headers: await headers() 
+    })
+    console.log(session, 'session')
+    if(session){
+        return NextResponse.next();
+    }
+    else{
+        return NextResponse.redirect(new URL('/auth/signin', request.url))
+    }
+    
+}
+
+// Alternatively, you can use a default export:
+// export default function proxy(request) { ... }
+
+export const config = {
+    matcher: ['/all-photos/:path*', '/profile'],
+}
